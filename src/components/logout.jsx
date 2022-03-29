2252442143bc3bb055b0c9f8b63eb0fc74f776ad
shahab.zamani <shahab.mirzamani@gmail.com>
2022-03-29 09:11:48 -0400

    logout add

---
+++ src/components/logout.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Logout extends Component {
+    // state = {  } 
+
+    componentDidMount(){
+        localStorage.removeItem('token');
+        window.location = '/';
+    }
+    render() { 
+        return null;
+    }
+}
+ 
+export default Logout;
